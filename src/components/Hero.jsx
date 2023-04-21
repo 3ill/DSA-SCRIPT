5d4414d038893f6138b362552fd10fcdd13d75dd
3illBaby <thrillanon@outlook.com>
2023-04-21 20:58:05 +0100

    Added animations to the hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,13 @@
 import { dsa } from "../assets";
+import { motion } from "framer-motion";
+import { slideAnimation, fadeAnimation } from "../utils/motion";
 
 const Hero = () => {
   const { headerWrapper, navWrapper, imageStyle, h2Style } = styles;
   const git = import.meta.env.VITE_GIT_URL;
   return (
     <header className={`${headerWrapper}`}>
-      <nav className={`${navWrapper}`}>
+      <motion.nav className={`${navWrapper}`} {...slideAnimation("left")}>
         <img src={dsa} alt="dsa_logo" className={`${imageStyle}`} />
 
         <button
@@ -15,16 +17,16 @@ const Hero = () => {
         >
           Github
         </button>
-      </nav>
+      </motion.nav>
       <h1 className="head_text">
         {" "}
         Summarize Articles with <br className="max-md:hidden" />
         <span className="purple_gradient">OPENAI GPT-4</span>
       </h1>
-      <h2 className={`${h2Style}`}>
+      <motion.h2 className={`${h2Style}`} {...fadeAnimation}>
         Introducing DsaScript, an innovative open-source article summarizer
         designed to simplify lengthy articles into concise and clear summaries.{" "}
-      </h2>
+      </motion.h2>
     </header>
   );
 };
@@ -32,6 +34,6 @@ const styles = {
   headerWrapper: "w-full flex justify-center items-center flex-col",
   navWrapper: "flex justify-between items-center flex-row w-full mb-10 pt-3",
   imageStyle: "w-16 object-contain rounded-md",
-  h2Style: "orange_gradient desc",
+  h2Style: " desc",
 };
 export default Hero;
