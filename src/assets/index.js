458055ded1edf9ef0f275fccee3a7e98095017a9
3illBaby <thrillanon@outlook.com>
2023-04-21 20:28:02 +0100

    Added the asset folder

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import linkIcon from "./link.svg";
+import loader from "./loader.svg";
+import copy from "./copy.svg";
+import logo from "./logo.svg";
+import tick from "./tick.svg";
+import dsa from "./dsa.jpeg";
+
+export { linkIcon, loader, copy, logo, tick, dsa };
